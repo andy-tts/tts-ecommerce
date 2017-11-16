b2074f784bb59847c4d97f19b3eb3f2ab0eb9112
Jade Kazoo <jadekzn@gmail.com>
2017-11-16 10:48:58 -0500

    Added seed info and formatting and categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+categories = [
+	Category.create(name: "Electronics"),
+	Category.create(name: "Furniture"),
+	Category.create(name: "Cooking"),
+	Category.create(name: "Pet Care"),
+	Category.create(name: "Entertainment")
+]
+
+
+	Product.create(name: "Pen", price: 1.20, quantity: 100, 
+		description: "An elegant writing tool", brand: "Bic", rating: 2, 
+		category: categories[4])
+	Product.create(name: "4k TV", price: 1200, quantity: 5, 
+		description: "Zone out in front of this TV", brand: "Vizio", rating: 4, 
+		category: categories[0])
+	Product.create(name: "Spatula", price: 4, quantity: 30, 
+		description: "An elegant cooking tool", brand: "Cuisinart", rating: 3, 
+		category: categories[2])
