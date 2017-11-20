f1711609be536bfecb3a37b0f10d9b1ee252e5ce
Jill R. Hayes <dvmjill@gmail.com>
2017-11-20 09:38:54 -0500

    create storefront controller and all items view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+  get 'storefront/all_items'
+
+  get 'items_by_category' => 'storefront#items_by_category'
+  get 'items_by_brand' => 'storefront#items_by_brand'
+
+
   devise_for :users
   resources :products
+  root 'storefront#all_items'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
