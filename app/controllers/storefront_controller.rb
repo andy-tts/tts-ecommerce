e8f0af915c4b32902524b407401737821c23e608
Andy TTS <awhillans@techtalentsouth.com>
2017-11-20 11:02:47 -0500

    Create partial for rendering products

--- app/controllers/storefront_controller.rb
+++ app/controllers/storefront_controller.rb
@@ -5,9 +5,10 @@ class StorefrontController < ApplicationController
 
   def items_by_category
   	@category = Category.find(params[:category_id])
- 
   end
 
   def items_by_brand
+  	@brand = params[:brand]
+  	@products = Product.where(brand: @brand)
   end
 end
