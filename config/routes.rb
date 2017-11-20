97bc70429fbff21e976455c7f0cb2fa7b79b2656
Jade Kazoo <jadekzn@gmail.com>
2017-11-20 09:38:41 -0500

    Created Storefront and the all_items page displays all the items and is also the root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  get 'storefront/all_items'
+  get 'storefront/items_by_brand'
+  get 'storefront/items_by_category'
+  
+  get 'items_by_category' => 'storefront#items_by_category'
+  get 'items_by_brand' => 'storefront#items_by_brand'
+
   devise_for :users
   resources :products
-  root 'products#index'
+  root 'storefront#all_items'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
