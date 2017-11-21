dd9fce8b5fdc1cc2dda326a8203cf40bfefcb5b9
Andy TTS <awhillans@techtalentsouth.com>
2017-11-21 12:05:02 -0500

    Use cancan to restrict access to destroying and editing products for guest users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,18 @@ class ApplicationController < ActionController::Base
 
   before_action :set_categories_and_brands
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   def set_categories_and_brands
     @categories = Category.all
     @brands = Product.pluck(:brand).sort.uniq
     @category_names_and_ids = @categories.collect do |category|
       [category.name, category.id]
     end
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:role])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:role])
   end
 end
